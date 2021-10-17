1f393158f7b8d4df65811637bbe54b78a6ebe31f
d00d1e <lee.julianay@gmail.com>
2021-10-16 21:22:45 -0700

    setup express server with socket.io

---
+++ server.js
@@ -0,0 +1,37 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Zoom Clone server running");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("self", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callended");
+  });
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  });
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server starte at http://localhost:${PORT}`);
+});
